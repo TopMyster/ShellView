93ef4d1f714dfb7d39ec58863b83e03b672cf6b4
TopMyster <topmyster@outlook.com>
2025-07-26 16:23:40 -0500

    tries to add both browser and iframe

--- script.js
+++ script.js
@@ -5,26 +5,41 @@ document.addEventListener('keydown', function(e) {
     if (e.key === 'Enter') {
         if (text.value.startsWith('help')) {
             view.src = 'commands.txt'
+            document.getElementById('view').style.display = 'block'
+            document.getElementById('browser').style.display = 'none'
         } else if (text.value.startsWith('to')) {
             view.src = text.value.slice(2)
+            document.getElementById('view').style.display = 'block'
+            document.getElementById('browser').style.display = 'none'
         } else if (text.value.startsWith('say')) {
             const txt = text.value.slice(3)
             const utterance = new SpeechSynthesisUtterance(txt)
             utterance.pitch = .5
+            document.getElementById('view').style.display = 'block'
+            document.getElementById('browser').style.display = 'none'
             window.speechSynthesis.speak(utterance)
         } else if (text.value.startsWith('to')) {
-           const target = text.value.slice(2).trim()
-           const fullPath = path.resolve('/', target)
-           view.src = 'file://' + fullPath
+           view.src = text.value.slice(2)
+           document.getElementById('view').style.display = 'block'
+           document.getElementById('browser').style.display = 'none'
         } else if (text.value.startsWith('time')) {
             view.src = 'home.html'
+            document.getElementById('view').style.display = 'block'
+            document.getElementById('browser').style.display = 'none'
         } else if (text.value.startsWith('ai')) {
-            view.src = 'https://www.perplexity.ai/search?q=' + text.value.slice(2)
+            document.getElementById('view').style.display = 'none'
+            document.getElementById('browser').style.display = 'block'
+            document.getElementById('browser').style.height = '300px';
+            document.getElementById('browser').src = 'https://www.perplexity.ai/search?q=' + text.value.slice(2)
         } else if (text.value.startsWith('time')) {
             view.src = 'home.html'
+            document.getElementById('view').style.display = 'block'
+            document.getElementById('browser').style.display = 'none'
         } 
           else {
             view.src = 'unknown.html'
+            document.getElementById('view').style.display = 'block'
+            document.getElementById('browser').style.display = 'none'
         }
     }
 })
