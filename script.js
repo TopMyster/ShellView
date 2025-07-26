b3a8dec7e03712f6926f482f445efcc72e5803f1
TopMyster <topmyster@outlook.com>
2025-07-25 22:43:35 -0500

    part 2

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ view = document.getElementById('view')
 
 document.addEventListener('keydown', function(e) {
     if (e.key === 'Enter') {
-        if (text.value === 'help') {
+        if (text.value.includes('help')) {
             view.src = '/executions/commands.txt'
-        } else if (text.value.includes('cd')) {
+        } else if (text.value.includes('to')) {
             view.src = text.value.slice(2)
         } else {
             view.src = '/executions/unknown.html'
